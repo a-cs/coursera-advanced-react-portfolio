a7d66133d30d4b129b43c0f1a8d079e2a47ca22e
Anderson Carneiro Sousa <acarneirosousa@gmail.com>
2023-07-10 11:03:46 -0300

    finishing projects card

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,22 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+	<VStack bg="white" color="black" align="start"  borderRadius="12px" spacing="12px">
+		<Image
+			borderRadius="12px"
+			boxSize='100%'
+			objectFit='cover'
+			src={imageSrc}
+			alt='Dan Abramov'
+		/>
+		<VStack align="start" spacing="12px" px="16px" pb="16px">
+			<Heading as='h5' size='sm' noOfLines={1}>{title}</Heading>
+			<Text>{description}</Text>
+			<Heading as='h6' size='xs' noOfLines={1}>See more <FontAwesomeIcon icon={faArrowRight} size="1x" /></Heading>
+		</VStack>
+	</VStack>
+  );
 };
 
 export default Card;
